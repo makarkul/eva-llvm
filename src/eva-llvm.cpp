e8ba37f44f25034e9bc785a2068593aa9153c5b1
Makarand Kulkarni <makarand.kulkarni@saankhyalabs.com>
2024-07-15 10:01:17 +0000

    Add skeleton code for Eva LLVM compiler

---
+++ src/eva-llvm.cpp
@@ -0,0 +1,26 @@
+/**
+ * Eva LLVM executable
+ */
+#include <string>
+
+#include "EvaLLVM.h"
+
+int main(int argc, char const *argv[]) {
+  /**
+   * Program to execute
+   */
+  std::string program = R"(
+    42
+  )";
+
+  /**
+   * Compiler instance.
+   */
+  EvaLLVM vm;
+
+  /** 
+   * Generate LLVM IR
+   */
+  vm.exec(program);
+  return 0;
+}
